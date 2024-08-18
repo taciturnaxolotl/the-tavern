3b35bd26f72cb1053146b6b5d33184b348983fae
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-18 16:12:25 -0400

    feat: track threads

--- features/mention.ts
+++ features/mention.ts
@@ -1,13 +1,29 @@
-import { slackApp } from '../index'
+import { prisma, slackApp } from '../index'
 import { blog } from '../lib/Logger'
 import { respond } from '../lib/quests'
 
 const mention = async () => {
     slackApp.event('app_mention', async ({ context, payload }) => {
+        if (
+            payload.user == undefined ||
+            payload.subtype != undefined ||
+            payload.thread_ts != undefined
+        )
+            return
+
         blog(
-            `${payload.user_profile?.display_name} triggered the app mention in ${payload.channel}`,
+            `${payload.user} triggered the app mention in ${payload.channel}`,
             'info'
         )
+
+        await prisma.threads.create({
+            data: {
+                userID: payload.user,
+                ts: payload.ts,
+                channel: payload.channel,
+            },
+        })
+
         await respond(context.say, payload)
     })
 }
--- features/thread.ts
+++ features/thread.ts
@@ -1,10 +1,19 @@
-import { slackApp } from '../index'
+import { prisma, slackApp } from '../index'
 import { blog } from '../lib/Logger'
 import { respond } from '../lib/quests'
 
 const mention = async () => {
     slackApp.anyMessage(async ({ context, payload }) => {
         if (payload.subtype != undefined || !payload.thread_ts) return
+
+        const thread = await prisma.threads.findFirst({
+            where: {
+                ts: payload.thread_ts,
+                channel: payload.channel,
+            },
+        })
+
+        if (!thread || thread.userID != payload.user) return
 
         blog(
             `${payload.user} triggered the message handler in ${payload.channel}`,
