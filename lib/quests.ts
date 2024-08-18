d985cef17671efd577999be6d414f6d3377a3b78
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-18 16:24:14 -0400

    feat: add message history to the bot

--- lib/quests.ts
+++ lib/quests.ts
@@ -5,15 +5,16 @@ import type {
     GenericMessageEvent,
 } from 'slack-edge'
 import { slackClient, openAIClient } from '..'
+import type { ChatCompletionMessageParam } from 'openai/resources/index.mjs'
 
 export async function respond(
     say: (
         params: Omit<ChatPostMessageRequest, 'channel'>
     ) => Promise<ChatPostMessageResponse>,
-    appMentionEvent: AppMentionEvent | GenericMessageEvent
+    event: AppMentionEvent | GenericMessageEvent
 ) {
     const initalMesssage = await say({
-        thread_ts: appMentionEvent?.ts,
+        thread_ts: event?.ts,
         text: 'thinking...',
         blocks: [
             {
@@ -26,19 +27,50 @@ export async function respond(
         ],
     })
 
+    let messages: ChatCompletionMessageParam[] = [
+        {
+            role: 'system',
+            content:
+                'You are a the propriator of the tavern and you are gruff and rude but eventually warm up to the party. Answer in no more than a paragraph',
+        },
+    ]
+
+    if (event.thread_ts != undefined) {
+        // get the contents of the thread
+        const threadContents = await slackClient.conversations.replies({
+            channel: event.channel,
+            ts: event.thread_ts,
+        })
+
+        for (const message of threadContents.messages!) {
+            // ignore the last message
+            if (message.ts == initalMesssage.ts) continue
+
+            if (message.user == event.user) {
+                messages.push({
+                    role: 'user',
+                    content: message.text!,
+                })
+            } else if ((message.bot_id = process.env.BOT_ID)) {
+                messages.push({
+                    role: 'assistant',
+                    content: message.text!,
+                })
+            }
+        }
+    } else {
+        messages.push({
+            role: 'user',
+            content: event.text!,
+        })
+    }
+
+    console.log(messages)
+
     const response = await openAIClient.chat.completions.create({
         model: 'gpt-4o-mini',
-        messages: [
-            {
-                role: 'system',
-                content:
-                    'You are a the propriator of the tavern and you are gruff and rude but eventually warm up to the party.',
-            },
-            {
-                role: 'user',
-                content: appMentionEvent?.text!,
-            },
-        ],
+        messages: messages,
+        max_tokens: 500,
     })
 
     await slackClient.chat.update({
